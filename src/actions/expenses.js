6da35013c2929de260d2eca2eddccf5e815f4038
rtheerdham <rohit.kdd@gmail.com>
2020-06-27 03:13:56 +0530

    remove/update db

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,13 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id }={})=>{
+  return(dispatch) => {
+  return database.ref(`expenses/${id}`).remove().then(()=>{
+      dispatch(removeExpense({id}))
+  });
+};
+};
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -39,6 +46,13 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+export const startEditExpense = (id,updates)=>{
+  return(dispatch) =>{
+    return database.ref(`expenses/${id}`).update(updates).then(()=>{
+      dispatch(editExpense({id,updates}))
+    });
+  };
+}
 export const setExpenses = (expenses) =>({
   type:'SET_EXPENSES',
   expenses
