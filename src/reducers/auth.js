dce6c9944c7b4ac9a7fb1d312adfb7e70b3af48c
rtheerdham <rohit.kdd@gmail.com>
2020-06-28 09:45:11 +0530

    final changes

---
+++ src/reducers/auth.js
@@ -0,0 +1,17 @@
+import { act } from "react-dom/test-utils";
+
+export default (state={},action)=>{
+    switch(action.type){
+        case 'LOGIN':
+            return{
+                uid:action.uid
+            }
+        case 'LOGOUT':
+            return{
+
+            }
+        default:
+            return state;
+    }
+
+}
